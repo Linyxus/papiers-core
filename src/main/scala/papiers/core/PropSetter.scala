82e31517dc21dd88bfdf86261acfcaec85c56718
Yichen Xu <yichen.x@outlook.com>
2021-08-07 18:48:04 +0800

    Add setter for authors

--- src/main/scala/papiers/core/PropSetter.scala
+++ src/main/scala/papiers/core/PropSetter.scala
@@ -18,12 +18,22 @@ object PropSetter:
   val titleSetter = new PropSetter {
     type V = String
 
-    val p: StrParser[String] = implicitly
+    val p: StrParser[V] = implicitly
 
     override def set(paper: Paper, title: String): Paper =
       paper.copy(title = title)
   }
 
+  val authorSetter = new PropSetter {
+    type V = List[AuthorName]
+
+    val p: StrParser[V] = implicitly
+
+    override def set(paper: Paper, authors: List[AuthorName]): Paper =
+      paper.copy(authors = authors)
+  }
+
   val allSetters: Map[String, PropSetter] = Map(
-    "title" -> titleSetter
+    "title" -> titleSetter,
+    "authors" -> authorSetter
   )
